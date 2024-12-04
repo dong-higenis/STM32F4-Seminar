d784d87382f1bba60c7c96db4fb2ca32299b37bc
DESKTOP-BF5T0JB\user <hgji@higenis.com>
2024-12-04 15:24:37 +0900

    실습 보드 v2 버전 x, y 버튼 핀 수정 반영

---
+++ firmware/stm32f411-lcd/App/ap/ap.h
@@ -0,0 +1,19 @@
+/*
+ * ap.h
+ *
+ *  Created on: Nov 26, 2024
+ *      Author: user
+ */
+
+#ifndef AP_AP_H_
+#define AP_AP_H_
+
+
+#include "ap_def.h"
+
+
+void apInit(void);
+void apMain(void);
+
+
+#endif /* AP_AP_H_ */
---
+++ firmware/stm32f411-lcd/App/hw/hw.h
@@ -0,0 +1,37 @@
+/*
+ * hw.h
+ *
+ *  Created on: Nov 26, 2024
+ *      Author: user
+ */
+
+#ifndef HW_H_
+#define HW_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "hw_def.h"
+
+#include "led.h"
+#include "uart.h"
+#include "cli.h"
+#include "buzzer.h"
+#include "gpio.h"
+#include "button.h"
+#include "adc.h"
+#include "st7789.h"
+#include "lcd.h"
+
+
+bool hwInit(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* HW_H_ */
